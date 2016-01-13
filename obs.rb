872a6643f274ff94cf1244df6a2f61a41a6c458d
Dan Kegel <dank@oblong.com>
2016-01-13 09:10:05 -0800

    Update obs to rel-0.6

--- obs.rb
+++ obs.rb
@@ -2,8 +2,8 @@ class Obs < Formula
   desc "Oblong Oblong Bootstrap Scripts"
   homepage "https://github.com/Oblong/obs"
   # Expanded by obs/buildshim, do not edit by hand
-  url "https://github.com/Oblong/obs/archive/rel-0.4.tar.gz"
-  sha256 "086c609d7d0c9d963811be492d29a7bc3060eda751c31725230b2c9fbcd6c061"
+  url "https://github.com/Oblong/obs/archive/rel-0.6.tar.gz"
+  sha256 "7b50463fabdfd582b6a5e6d6fc0857715fa28290ba68b5a3bf167f6c4d806dc4"
   head "https://github.com/Oblong/obi.git", :using => :git, :branch => "master"
 
   def install
