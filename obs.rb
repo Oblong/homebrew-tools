a70277b671c8d918592fe99b8f7c04803f02b867
Buildus Bottus <buildbot@rbb-osx1011-3.la923.oblong.net>
2016-09-27 12:05:15 -0700

    Update obs to rel-0.21

--- obs.rb
+++ obs.rb
@@ -2,8 +2,8 @@ class Obs < Formula
   desc "Oblong Oblong Bootstrap Scripts"
   homepage "https://github.com/Oblong/obs"
   # Expanded by obs/buildshim, do not edit by hand
-  url "https://github.com/Oblong/obs/archive/rel-0.20.tar.gz"
-  sha256 "d7a687d5752e8eaab38e0ebf7890ebb651e82a632759b937f719339d28e5a693"
+  url "https://github.com/Oblong/obs/archive/rel-0.21.tar.gz"
+  sha256 "046cf4d3d863ea682f0adce8a81fb5654162299c13df365032cd761727975ae2"
   head "https://github.com/Oblong/obi.git", :using => :git, :branch => "master"
 
   def install
