469bfcf6e97910d4b54e8cc78f990337fd178280
Buildus Bottus <buildbot@rbb-osx1011-1.oblong.com>
2017-02-15 22:27:58 -0800

    Update obs to rel-0.31

--- obs.rb
+++ obs.rb
@@ -2,8 +2,8 @@ class Obs < Formula
   desc "Oblong Oblong Bootstrap Scripts"
   homepage "https://github.com/Oblong/obs"
   # Expanded by obs/buildshim, do not edit by hand
-  url "https://github.com/Oblong/obs/archive/rel-0.30.tar.gz"
-  sha256 "30f441d8629852f7d22f1d5509a467e0902f292000a165a231fda12ff059915f"
+  url "https://github.com/Oblong/obs/archive/rel-0.31.tar.gz"
+  sha256 "61b13fe5db290c54466898afe1e91063fd19e5dd6692939cf72d8bdbea6187cd"
   head "https://github.com/Oblong/obi.git", :using => :git, :branch => "master"
 
   def install
