0bdf985d4401a93f98b7828334d1a7f1f80d4f9b
Dan Kegel <dank@oblong.com>
2017-03-24 09:35:08 -0700

    Update spruce to rel-0.7

--- spruce.rb
+++ spruce.rb
@@ -2,8 +2,8 @@ class Spruce < Formula
   desc "Oblong clang-format helper"
   homepage "https://github.com/Oblong/spruce"
   # Expanded by spruce/buildshim, do not edit by hand
-  url "https://github.com/Oblong/spruce/archive/rel-0.5.tar.gz"
-  sha256 "5bb5ed34cbd35ace885edfd7f550bd67d40de3c2a33badfd76615c40eff7a3dd"
+  url "https://github.com/Oblong/spruce/archive/rel-0.7.tar.gz"
+  sha256 "8303bd5a4f0ce55d9b2ba55faa1f45fa68f2e23190d7e59ad71142f3dd15d0b6"
   head "https://github.com/Oblong/spruce.git", :using => :git, :branch => "master"
 
   def install
