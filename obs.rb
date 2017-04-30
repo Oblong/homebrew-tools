23c92bd74a6674c574f7969aaa717f6fccb1d62e
Dan Kegel <dank@oblong.com>
2017-04-30 15:27:41 -0700

    Update obs to rel-0.39

--- obs.rb
+++ obs.rb
@@ -2,8 +2,8 @@ class Obs < Formula
   desc "Oblong Bootstrap Scripts"
   homepage "https://github.com/Oblong/obs"
   # Expanded by obs/buildshim, do not edit by hand
-  url "https://github.com/Oblong/obs/archive/rel-0.38.tar.gz"
-  sha256 "39060516a0a9d1bc631bcf4c110774e1801be0e0361a8b1b3e06e276f4ff303a"
+  url "https://github.com/Oblong/obs/archive/rel-0.39.tar.gz"
+  sha256 "22d809b2567e4ed4868cf5b12c6ae6a73e31f1169d431f0a2a22fa2218acb3c4"
   head "https://github.com/Oblong/obs.git", :using => :git, :branch => "master"
 
   def install
