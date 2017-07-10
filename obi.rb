78925215b703a74d58c611d63c023a793667d99f
Aaron Rice <arice@oblong.com>
2017-07-10 11:57:13 -0700

    fix sha256 for obi v3.4.2

--- obi.rb
+++ obi.rb
@@ -2,7 +2,7 @@ class Obi < Formula
   desc "Command-line tool for scaffolding and running g-speak projects"
   homepage "https://github.com/Oblong/obi"
   url "https://github.com/Oblong/obi/archive/v3.4.2.tar.gz"
-  sha256 "880336ffe469de07f5e7e8f83426b650780c6ec4ba4b6e0be3215ef693fd1e14"
+  sha256 "fd5e41cb25204a44b61d6054ced2465d5db6b4d5e3078b56e03823e1d6ac9e81"
   head "https://github.com/Oblong/obi.git", :using => :git, :branch => "master"
 
   depends_on :python if MacOS.version <= :snow_leopard
