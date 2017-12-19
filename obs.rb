724b2d2a92f2c1e30726aac42a693cfd687d95a5
Dan Kegel <dank@oblong.com>
2017-12-19 09:38:15 -0800

    obs: need to build before installing; add missing dependency on asciidoc
    now that obs and bau are generated, have to do 'make' before 'make install'.
    
    Also, building the man page requires asciidoc, so declare that dependency.

--- obs.rb
+++ obs.rb
@@ -1,12 +1,15 @@
 class Obs < Formula
   desc "Oblong Bootstrap Scripts"
   homepage "https://github.com/Oblong/obs"
+  depends_on "asciidoc"
+
   # Expanded by obs/buildshim, do not edit by hand
   url "https://github.com/Oblong/obs/archive/rel-1.3.tar.gz"
   sha256 "e436a73c2c480c0928caa1980102ff0f11f836c7d0c0573be08810cea9c59235"
   head "https://github.com/Oblong/obs.git", :using => :git, :branch => "master"
 
   def install
+    system "make"
     system "make", "install", "PREFIX=#{prefix}"
   end
 end
