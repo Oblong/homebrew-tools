69959e18b12027052ed011d392909100bf7c03a5
Dan Kegel <dank@oblong.com>
2018-01-22 14:15:36 -0800

    Update obs to rel-1.8

--- obs.rb
+++ obs.rb
@@ -5,8 +5,8 @@ class Obs < Formula
   homepage "https://github.com/Oblong/obs"
   depends_on "asciidoc"
 
-  url "https://github.com/Oblong/obs/archive/rel-1.7.tar.gz"
-  sha256 "c89c0841857991d109c369fc8b0447c9e46a5e665704ee2c3a0ba4a3d1f8cb4c"
+  url "https://github.com/Oblong/obs/archive/rel-1.8.tar.gz"
+  sha256 "8b6cad4c4194806a82b7f45645bf6ada6bab09356d06cf1a5be1fc3960f0a512"
   head "https://github.com/Oblong/obs.git", :using => :git, :branch => "master"
 
   def install
