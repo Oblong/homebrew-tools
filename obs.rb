33d4eed5bc39d3359c588d83d66641310777367e
Dan Kegel <dank@oblong.com>
2018-02-01 18:28:42 -0800

    Update obs to rel-1.11

--- obs.rb
+++ obs.rb
@@ -5,9 +5,10 @@ class Obs < Formula
   homepage "https://github.com/Oblong/obs"
   depends_on "asciidoc"
   depends_on "coreutils"
+  depends_on "gnu-sed"
 
-  url "https://github.com/Oblong/obs/archive/rel-1.10.tar.gz"
-  sha256 "c7d53d80a7f4d984d38d639b5156033f52820252f954b2bc42db4706b5f41ddd"
+  url "https://github.com/Oblong/obs/archive/rel-1.11.tar.gz"
+  sha256 "51b7321096819da2d9eb8eb17e1d9f995e5016e9001347a04078186bfbd73648"
   head "https://github.com/Oblong/obs.git", :using => :git, :branch => "master"
 
   def install
