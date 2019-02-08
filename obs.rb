51514f25a24b3de288efa7b57300eef0d2c4c4d2
Dan Kegel <dank@oblong.com>
2019-02-08 15:16:11 -0800

    Update obs to rel-1.29

--- obs.rb
+++ obs.rb
@@ -7,8 +7,8 @@ class Obs < Formula
   depends_on "coreutils"
   depends_on "gnu-sed"
 
-  url "https://github.com/Oblong/obs/archive/rel-1.28.tar.gz"
-  sha256 "a347a1dbd42798a4dbe8374a041277eb81ecf84631144f0c457374f5835d6890"
+  url "https://github.com/Oblong/obs/archive/rel-1.29.tar.gz"
+  sha256 "6f44b92a2bd763e4dcd9e23139d1dfccc068891b692700f1509d71f687f2e86a"
   head "https://github.com/Oblong/obs.git", :using => :git, :branch => "master"
 
   def install
